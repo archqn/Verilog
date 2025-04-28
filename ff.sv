03878b8b0e74145e72ee3db0609408754293b9fe
Lucas Jia <lucasjia@ucsb.edu>
2025-04-28 12:34:20 -0700

    ff 3 cycle fix

--- ff.sv
+++ ff.sv
@@ -8,19 +8,41 @@ module ff_reference #(
     output logic [WIDTH-1:0] data_o
 );
 
-    logic [WIDTH-1:0] data_d, data_q;
-    assign data_o = data_q;
-    always_comb begin : data_set
-        // data_d is set by combinational logic
-        data_d = data_i;
-    end
-    // async reset
-    always_ff @(posedge clk or posedge rst) begin : data_ff
+    logic [WIDTH-1:0] data_d, data_q1, data_q2, data_q3;
+      
+    // ff1 stores i in data_q1
+    always_ff @(posedge clk or posedge rst) begin : data_ff1
         if (rst) begin
-            data_q <= '0;
+            data_q1 <= '0;
         end else begin
-            data_q <= data_d;
+            data_q1 <= data_i;
         end
     end
-
+      
+    // comb logic between ff1 & ff2: data_q1 => data_d
+    always_comb begin : data_set
+        data_d = data_q1;
+    end
+    
+    // ff2 stores d in q2
+    always_ff @(posedge clk or posedge rst) begin : data_ff2
+        if (rst) begin
+            data_q2 <= '0;
+        end else begin
+            data_q2 <= data_d;
+        end
+    end
+  
+  	// ff3 stores data_q2 in data_q3
+    always_ff @(posedge clk or posedge rst) begin : data_ff3 
+      if (rst) begin
+        data_q3 <= '0;
+      end else begin
+        data_q3 <= data_q2;
+      end
+    end
+    
+    // wire connects data_q3 to data_o 
+    assign data_o = data_q3;
+    
 endmodule
